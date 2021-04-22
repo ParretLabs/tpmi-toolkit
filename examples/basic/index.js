d3340083d3ab46bfd46fdf76e7f3fadc14e0cea2
ParretLabs <parretlabs@gmail.com>
2021-04-21 20:04:57 -0700

    Updated examples

--- examples/basic/index.js
+++ examples/basic/index.js
@@ -7,14 +7,14 @@ const writeFile = util.promisify(fs.writeFile);
 const { Vectorizer, MapUtilities } = require('../../');
 
 (async () => {
-	console.log("Input a valid U-M map ID");
+	console.log("TPMI Toolkit - Basic");
 
 	prompt.start();
-	const {mapID} = await prompt.get(['mapID']);
+	const {UMmapID} = await prompt.get(['UMmapID']);
 
 	console.log("Retrieving map...");
 
-	let tileMap = await MapUtilities.mapIDToTileMap(mapID);
+	let tileMap = await MapUtilities.mapIDToTileMap(UMmapID);
 	let wallMap = MapUtilities.tileMapToWallMap(tileMap);
 
 	let vectorMap = Vectorizer.createVectorMapFromTileMap(tileMap);
--- examples/randomizer/index.js
+++ examples/randomizer/index.js
@@ -6,24 +6,50 @@ const writeFile = util.promisify(fs.writeFile);
 
 const { Vectorizer, Randomizer, MapUtilities, Flatten } = require('../../');
 
+const promptSchema = {
+	properties: {
+		mapID: {
+			description: 'Unfortunate Maps ID',
+			type: 'number',
+			pattern: /^[0-9]+$/,
+			message: 'ID can only contain digits',
+			default: 5436
+		},
+		noiseLevel: {
+			description: 'Noise Level',
+			type: 'number',
+			pattern: /^[0-9]+$/,
+			message: 'Noise must be a number',
+			default: 1
+		},
+		seed: {
+			description: 'Randomization Seed',
+			type: 'number',
+			pattern: /^[0-9]+$/,
+			message: 'Seed must be a number'
+		}
+	}
+};
+
 (async () => {
-	console.log("Input a valid U-M map ID");
+	console.log("TPMI Toolkit - Randomizer - Wall Randomizer");
 
 	prompt.start();
-	const {mapID} = await prompt.get(['mapID']);
+	const settings = await prompt.get(promptSchema);
+	settings.noiseLevel = Number(settings.noiseLevel);
 
 	console.log("Retrieving map...");
-	let tileMap = await MapUtilities.mapIDToTileMap(mapID);
+	let tileMap = await MapUtilities.mapIDToTileMap(settings.mapID);
 
 	let vectorMapOld = Vectorizer.createVectorMapFromTileMap(tileMap);
 	let vectorMapNew = Vectorizer.createVectorMapFromTileMap(tileMap);
 
-	let mapRandomizer = new Randomizer();
+	let mapRandomizer = new Randomizer(settings.seed);
 
 	vectorMapNew.walls = mapRandomizer.additiveRandomizeWalls(
 		vectorMapNew.walls,
-		new Flatten.Vector(-2, -2),
-		new Flatten.Vector(2, 2)
+		new Flatten.Vector(-settings.noiseLevel, -settings.noiseLevel),
+		new Flatten.Vector(settings.noiseLevel, settings.noiseLevel)
 	);
 
 	await writeFile(__dirname + "/map.html", vectorMapOld.visualize() + vectorMapNew.visualize());
